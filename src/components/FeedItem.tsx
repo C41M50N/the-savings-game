d83ff5be07be5d28e4bf145104fc37f63fc7dd4b
Charles Buffington II <charles.fsb2@gmail.com>
2022-08-04 16:30:31 -0400

    small styling adjustments

--- src/components/FeedItem.tsx
+++ src/components/FeedItem.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import { Container, Title, Text, Grid, Divider } from '@mantine/core'
 import { UserCircle } from 'tabler-icons-react'
@@ -9,20 +10,20 @@ type Props = {
 
 const FeedItem = ({ feedEntry }: Props) => {
 
-    return (
-        <Container style={{ width: '100%' }}>
-            <Grid>
-                <Grid.Col span={1}>
-                    <UserCircle size={30} />
-                </Grid.Col>
-                <Grid.Col span={4}>
-                    <Title order={3}>{feedEntry.user}</Title>
-                </Grid.Col>
-            </Grid>
-            <Text size="md">{feedEntry.feedMessage}</Text>
-            <Divider size="md"/>
-        </Container>
-    );
+	return (
+		<Container style={{ width: '100%' }}>
+			<Grid>
+				<Grid.Col span={1}>
+					<UserCircle size={30} />
+				</Grid.Col>
+				<Grid.Col span={4}>
+					<Title order={3}>{feedEntry.user}</Title>
+				</Grid.Col>
+			</Grid>
+			<Text size="md">{feedEntry.feedMessage}</Text>
+			<Divider size="md" />
+		</Container>
+	);
 }
 
 export default FeedItem
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react'
 import { Container, Title, Text, Grid, Stack, Button, Collapse, Avatar, Divider, Progress } from '@mantine/core'
 import { CaretLeft, CaretDown, UserCircle } from 'tabler-icons-react'
@@ -8,52 +9,52 @@ type Props = {
 }
 
 const Profile = ({ user }: Props) => {
-    const [opened, setOpened] = useState(true);
-    const totalCurrentAmount = user.objectives.reduce((total, currentValue) => total = total + currentValue.currentAmount,0);
-    const totalGoalAmount = user.objectives.reduce((total, currentValue) => total = total + currentValue.goalAmount,0);
-    const overallProgressPercent = Number.parseFloat((totalCurrentAmount / totalGoalAmount * 100).toFixed(0));
+	const [opened, setOpened] = useState(true);
+	const totalCurrentAmount = user.objectives.reduce((total, currentValue) => total = total + currentValue.currentAmount, 0);
+	const totalGoalAmount = user.objectives.reduce((total, currentValue) => total = total + currentValue.goalAmount, 0);
+	const overallProgressPercent = Number.parseFloat((totalCurrentAmount / totalGoalAmount * 100).toFixed(0));
 
-    return (
-        <Container>
-            <Grid>
-                <Grid.Col span={9}>
-                    <Title order={2}>Profile</Title>
-                </Grid.Col>
-                <Grid.Col span={3}>
-                    <Button variant='default' onClick={() => setOpened((o) => !o)}>
-                        {opened ? <CaretDown /> : <CaretLeft />}
-                    </Button>
-                </Grid.Col>
-            </Grid>
-            <Collapse in={opened} style={{ border: "2px solid gray", padding: "15px", marginTop: "10px" }}>
-                <Grid style={{ paddingBottom: 15 }}>
-                    <Grid.Col span={3}>
-                        <Avatar radius="xl" src={user.avatar != "" ? user.avatar : null} alt="no image here" />
-                    </Grid.Col>
-                    <Grid.Col span={9}>
-                        <Title order={3}>{user.name}</Title>
-                    </Grid.Col>
-                </Grid>
-                <Divider size="md" />
-                <Container px={0} style={{ paddingTop: "10px", textAlign: "center" }}>
-                    <Title order={4} align={'center'}>STAT</Title>
-                    <Text size="lg">
-                        <strong>{user.objectives.length}</strong> active objectives
-                    </Text>
-                    <Text size="lg">
-                        <strong>${totalCurrentAmount}</strong> saved in total
-                    </Text>
-                    <Text size="lg">
-                        <strong>${totalGoalAmount}</strong> to be saved
-                    </Text>
-                    <Text size="lg">
-                        Overall Progress <strong>{overallProgressPercent}%</strong>
-                        <Progress value={overallProgressPercent} color={'blue'} size="xl" animate style={{ marginTop: "15px", marginLeft: "10%", width: '80%', height: 16 }} />
-                    </Text>
-                </Container>
-            </Collapse>
-        </Container>
-    )
+	return (
+		<Container>
+			<Grid>
+				<Grid.Col span={9}>
+					<Title order={2}>Profile</Title>
+				</Grid.Col>
+				<Grid.Col span={3}>
+					<Button variant='default' onClick={() => setOpened((o) => !o)}>
+						{opened ? <CaretDown /> : <CaretLeft />}
+					</Button>
+				</Grid.Col>
+			</Grid>
+			<Collapse in={opened} style={{ border: "2px solid gray", borderRadius: 10, padding: "15px", marginTop: "10px" }}>
+				<Grid style={{ paddingBottom: 15 }}>
+					<Grid.Col span={1}>
+						<Avatar radius="xl" src={user.avatar != "" ? user.avatar : null} alt="no image here" />
+					</Grid.Col>
+					<Grid.Col span={10}>
+						<Title order={3} style={{ marginLeft: 10 }}>{user.name}</Title>
+					</Grid.Col>
+				</Grid>
+				<Divider size="md" />
+				<Container px={0} style={{ paddingTop: "10px", textAlign: "center" }}>
+					<Title order={4} align={'center'}>STAT</Title>
+					<Text size="lg">
+						<strong>{user.objectives.length}</strong> active objectives
+					</Text>
+					<Text size="lg">
+						<strong>${totalCurrentAmount}</strong> saved in total
+					</Text>
+					<Text size="lg">
+						<strong>${totalGoalAmount}</strong> to be saved
+					</Text>
+					<Text size="lg">
+						Overall Progress <strong>{overallProgressPercent}%</strong>
+						<Progress value={overallProgressPercent} color={'blue'} size="xl" animate style={{ marginTop: "15px", marginLeft: "10%", width: '80%', height: 16 }} />
+					</Text>
+				</Container>
+			</Collapse>
+		</Container>
+	)
 }
 
 export default Profile
